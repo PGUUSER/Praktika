2706ac6ff668fcd57b9d43ee921198b288e46e89
Artem <kachkurkinartem1@gmail.com>
2025-07-04 17:09:09 +0300

    task:#1 Алгоритм поразрядной сортировки

---
+++ practics/practics/practics.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h>
+#include <string.h>
+#include <limits.h>
+
+// LSD Radix Sort
+void radixSortLSD(int* arr, int n) {
+    int max = getMax(arr, n);
+    for (int exp = 1; max / exp > 0; exp *= 10) {
+        countingSortLSD(arr, n, exp);
+    }
+}
+
+// MSD Radix Sort
+void radixSortMSD(int* arr, int n) {
+    int max = getMax(arr, n);
+    int exp = 1;
+    while (max / exp > 0) {
+        exp *= 10;
+    }
+    exp /= 10;
+    radixSortMSDHelper(arr, 0, n - 1, exp);
+}
