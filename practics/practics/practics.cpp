8b956993802126024b8fc8bb77a72d8d5509a410
Artem <kachkurkinartem1@gmail.com>
2025-07-05 14:52:59 +0300

    task:#2 Работа с файлами

--- practics/practics/practics.cpp
+++ practics/practics/practics.cpp
@@ -1,10 +1,25 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+﻿﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <locale.h>
 #include <string.h>
 #include <limits.h>
+
+void saveToFile(int* arr, int n, const char* filename) {
+    FILE* file = fopen(filename, "w");
+    if (file == NULL) {
+        printf("Ошибка при открытии файла для записи!\n");
+        return;
+    }
+    fprintf(file, "%d\n", n);
+    for (int i = 0; i < n; i++) {
+        fprintf(file, "%d ", arr[i]);
+    }
+    fprintf(file, "\n");
+    fclose(file);
+    printf("Массив сохранён в файл: %s\n", filename);
+}
 
 // LSD Radix Sort
 void radixSortLSD(int* arr, int n) {
@@ -24,3 +39,30 @@ void radixSortMSD(int* arr, int n) {
     exp /= 10;
     radixSortMSDHelper(arr, 0, n - 1, exp);
 }
+void manualInput(int** arr, int* n) {
+    printf("Введите количество элементов: ");
+    scanf("%d", n);
+    *arr = (int*)malloc(*n * sizeof(int));
+    printf("Введите %d чисел:\n", *n);
+    for (int i = 0; i < *n; i++) {
+        scanf("%d", &(*arr)[i]);
+    }
+    saveToFile(*arr, *n, "input.txt");
+}
+
+void randomInput(int** arr, int* n) {
+    int min, max;
+    printf("Введите количество элементов: ");
+    scanf("%d", n);
+    printf("Введите минимальное значение: ");
+    scanf("%d", &min);
+    printf("Введите максимальное значение: ");
+    scanf("%d", &max);
+    *arr = (int*)malloc(*n * sizeof(int));
+    srand(time(NULL));
+    for (int i = 0; i < *n; i++) {
+        (*arr)[i] = rand() % (max - min + 1) + min;
+    }
+    saveToFile(*arr, *n, "input.txt");
+}
+
